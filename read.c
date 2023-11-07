a6e6cb44bbd761c755b2e04d74842f765a90445a
Eric <hayter.eric@gmail.com>
2023-11-06 21:10:49 -0500

    reading file now moved into function and created masking function to combine 2 bytes for an instruction

--- read.c
+++ read.c
@@ -4,26 +4,45 @@
 #define ADDRESS_OFFSET 0x200
 #define INSTRUCTION_SIZE 0xFFF - ADDRESS_OFFSET
 
-uint8_t instructions[INSTRUCTION_SIZE]; // used to store the game instructions
+int readinstructions(const char *filename, uint8_t *instructions);
+uint16_t createinstruction(uint8_t b1, uint8_t b0);
 
-FILE *fp;
-int filesize;
-
-int main()
+int main(int argc, char **argv)
 {
-    fp = fopen("Tetris.ch8", "r"); 
+    char *filename = argv[1];
+    uint8_t instructions[INSTRUCTION_SIZE]; // 0-255 values
+    if (readinstructions(filename, instructions)) // reading failed
+        return 1;
+    for (int i = 0; i < 20; i++) {
+        printf("%02X\n", instructions[i]);
+    }
+}
 
-    fseek(fp, 0L, SEEK_END); // find the size of the file
-    filesize = ftell(fp);
+uint16_t createinstruction(uint8_t b1, uint8_t b0)
+{
+    uint16_t instruction = b1;
+    instruction <<= 8;
+    return instruction + b0;
+}
+
+
+int readinstructions(const char *filename, uint8_t *instructions)
+{
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("error: could not read file %s", filename);
+        return 1;
+    }
+
+    fseek(fp, 0L, SEEK_END);
+    long filesize = ftell(fp);
     fseek(fp, 0L, SEEK_SET);
 
-    int i = 0;
-    uint8_t c = fgetc(fp);
-    
-    for (int i = 0; i < filesize; ++i) {
+    uint8_t c;
+    for (long i = 0L; i < filesize; i++) {
         instructions[i] = fgetc(fp);
     }
 
-
+    fclose(fp);
     return 0;
 }
